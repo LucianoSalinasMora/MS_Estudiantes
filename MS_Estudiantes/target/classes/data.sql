95130fd8959ff47cccadcf1d6778ab0754a118e6
LucianoSalinasMora <luci.salinasm@duocuc.cl>
2026-06-22 23:27:39 -0400

    Delete MS_Estudiantes directory

--- MS_Estudiantes/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO estudiantes (rut, nombres, apellidos, email, telefono, fecha_nacimiento, fecha_ingreso, direccion, estado_matricula)
-VALUES ('20.123.456-7', 'Diego Andrés', 'Mendoza Silva', 'diego.mendoza@alumnos.cl', '+56987654321', '2003-05-14', '2023-03-06', 'Av. Libertador Bernardo O Higgins 1450, Santiago', 'VIGENTE')
-ON DUPLICATE KEY UPDATE rut=rut;
